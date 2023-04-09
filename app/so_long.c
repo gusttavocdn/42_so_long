3c21cf985022da8cf4b526fb6f9742c220abfd48
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2023-04-08 21:23:18 -0300

    chore: update CMAKE to debug project

--- app/so_long.c
+++ app/so_long.c
@@ -12,35 +12,12 @@
 
 #include "../include/so_long.h"
 
-char	**read_xpm_image(void)
-{
-	int		fd;
-	char	*line;
-	char	**data;
-
-	fd = open("../assets/block.xpm", O_RDONLY);
-	line = get_next_line(fd);
-	while (line != NULL)
-	{
-		if (ft_strnstr(line, "static char", 50))
-		{
-			data = ft_split(line, ',');
-			break ;
-		}
-		free(line);
-		line = get_next_line(fd);
-	}
-	close(fd);
-	return (data);
-}
-
 int	main(void)
 {
 	t_mlx	mlx;
 
 	mlx.mlx_ptr = mlx_init();
 	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, W_WIDTH, W_HEIGHT, "So Long");
-	read_xpm_image();
 	mlx_key_hook(mlx.win_ptr, handle_key_events, &mlx);
 	mlx_loop(mlx.mlx_ptr);
 	return (0);
